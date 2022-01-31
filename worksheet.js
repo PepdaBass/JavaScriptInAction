379a670116c924f89b177e95e0bf502222e56b9a
Jeffrey Peplinski <jeffreypeplinski@jeffreys-mbp.lan>
2022-01-31 15:49:10 -0500

    Completed conditionals problems and first problem of loops.

--- worksheet.js
+++ worksheet.js
@@ -10,32 +10,61 @@
 
 // console.log("I can't wait for " + dayOfWeek + "!");
 
-// #2. User Input
+// // #2. User Input
 
-let animalInput = prompt("What is your favorite animal?")
-let colorInput = prompt("What is your favorite color?")
+// let animalInput = prompt("What is your favorite animal?")
+// let colorInput = prompt("What is your favorite color?")
 
-console.log("I've never seen a " + colorInput + " " + animalInput + "!")
+// console.log("I've never seen a " + colorInput + " " + animalInput + "!")
 
-// CONDITIONALS
+// // CONDITIONALS
 
 // #1. Meals
 // Favorites: Breakfast Burrito, Grilled Cheese Sandwich & Soup, Homemade Pizza
 
-let timeOfDay = 1300;
-let favoriteBreakfastLunchOrDinner;
+// let timeOfDay = 1700;
+// let favoriteBreakfastLunchOrDinner;
 
-if(timeOfDay < 1200) {
-    favoriteBreakfastLunchOrDinner = "Breakfast Burrito";
-    console.log("My favorite breakfast is: " + favoriteBreakfastLunchOrDinner + ".")
-}
+// if(timeOfDay < 1200) {
+//     favoriteBreakfastLunchOrDinner = "Breakfast Burrito";
+//     console.log("My favorite breakfast is: " + favoriteBreakfastLunchOrDinner + ".")
+// }
 
-else if(timeOfDay >= 1200 && timeOfDay < 1700) {
-    favoriteBreakfastLunchOrDinner = "Grilled Cheese Sandwich & Soup";
-    console.log("My favorite lunch is " + favoriteBreakfastLunchOrDinner + ".");
-}
+// else if(timeOfDay >= 1200 && timeOfDay < 1700) {
+//     favoriteBreakfastLunchOrDinner = "Grilled Cheese Sandwich & Soup";
+//     console.log("My favorite lunch is: " + favoriteBreakfastLunchOrDinner + ".");
+// }
 
-else {
-    favoriteBreakfastLunchOrDinner = "Homemade Pizza";
-    console.log("My favorite dinner is " + favoriteBreakfastLunchOrDinner + ".")
+// else if(timeOfDay >= 1700) {
+//     favoriteBreakfastLunchOrDinner = "Homemade Pizza";
+//     console.log("My favorite dinner is: " + favoriteBreakfastLunchOrDinner + ".")
+// }
+
+// // #2. Random Number
+
+// randomNumber = Math.floor(Math.random() * 11);
+
+// if(randomNumber >= 0 && randomNumber <= 2) {
+//     console.log("Beatles");
+// }
+
+// else if(randomNumber >=3 && randomNumber <= 5) {
+//     console.log("Stones");
+// }
+
+// else if(randomNumber >= 6 && randomNumber <= 8) {
+//     console.log("Floyd");
+// }
+
+// else {
+//     console.log("Hendrix");
+// }
+
+
+// LOOPS
+
+// #1. JavaScript is Cool
+
+for(let i = 0; i < 7; i++) {
+    console.log("JavaScript is cool!")
 }
